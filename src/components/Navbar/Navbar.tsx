71b4d4eaa25f5f7fa01200f2e1dad1bc27061001
Sahil Padvi <>
2023-11-05 23:03:50 +0530

    feat: initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+export const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="" alt="logo" className="logo" />
+        <p>Course4U</p>
+      </div>
+      <div className="search__container"></div>
+    </nav>
+  );
+};
---
+++ src/components/index.component.ts
@@ -0,0 +1,2 @@
+export { Navbar } from "./Navbar/Navbar";
+export { Layout } from "./Layout/Layout";
